abc470aec5d4dab52731db2dd4c6453fd0604ba9
hmtcelik <abhmtcelik@gmail.com>
2023-12-01 21:06:05 +0300

    create templates and link with views

--- food/urls.py
+++ food/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, FoodListView, FoodDetailView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("food/", FoodListView.as_view(), name="food_list"),
+    path("food/<int:pk>/", FoodDetailView.as_view(), name="food_detail"),
 ]
--- food/views.py
+++ food/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import Food
 
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+
+# list view food
+class FoodListView(generic.ListView):
+    model = Food
+    template_name = "food_list.html"
+    context_object_name = "foods"
+
+    def get_queryset(self):
+        return Food.objects.all()[:9]
+
+
+class FoodDetailView(generic.DetailView):
+    model = Food
+    template_name = "food_detail.html"
+    context_object_name = "food"
