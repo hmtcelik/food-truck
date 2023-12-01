751a79da9fc15e015de4cc8e7c07af00849ca176
hmtcelik <abhmtcelik@gmail.com>
2023-12-01 20:19:11 +0300

    fix: initial script key errors

--- food/management/commands/load_foods.py
+++ food/management/commands/load_foods.py
@@ -6,19 +6,19 @@ import pandas as pd
 
 class Command(BaseCommand):
     def handle(self, **options):
-        df = pd.read_csv("./utils/data/food-truck-data.csv").first(1000)
+        df = pd.read_csv("./utils/data/food-truck-data.csv").head(1000)
 
         with transaction.atomic():
             for _, row in df.iterrows():
                 food = Food(
-                    location_id=row["location_id"],
-                    applicant=row["applicant"],
-                    facility_type=row["facility_type"],
-                    location_description=row["location_description"],
-                    address=row["address"],
-                    food_items=row["food_items"],
-                    latitude=row["latitude"],
-                    longitude=row["longitude"],
+                    location_id=row["locationid"],
+                    applicant=row["Applicant"],
+                    facility_type=row["FacilityType"],
+                    location_description=row["LocationDescription"],
+                    address=row["Address"],
+                    food_items=row["FoodItems"],
+                    latitude=row["Latitude"],
+                    longitude=row["Longitude"],
                 )
                 food.save()
 
